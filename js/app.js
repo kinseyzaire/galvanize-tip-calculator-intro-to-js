3203af491d5f41184892a312a573e60a6a83b8b9
Kinsey Ashby <kinseyzaire@gmail.com>
2015-11-21 13:17:44 -0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// addEventListener
+// look at the form input for price
+
+var price = document.getElementById('price')
+   // make it a number not a string
+   // take out symbols
+// look at the form input for amount
+var amount = document.getElementById('amount')
+// calculate
+calculator(price , amount)
+// return tip
